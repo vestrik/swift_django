bfc01febca831683e10d5a38861bfb684601718a
m.golovanov <m.golovanov@uk-swift.ru>
2025-05-26 12:01:07 +0300

    fix logout

--- swift_django/accounts/views.py
+++ swift_django/accounts/views.py
@@ -18,7 +18,8 @@ def login_view(request):
 
 
 def logout_view(request):
-    if request.method == 'POST':
+    print(request.method)
+    if request.method == 'GET':
         logout(request)
         return redirect('calculation_sheet:home')
     else:
