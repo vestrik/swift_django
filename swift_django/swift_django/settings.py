5110593aa0e0dfc8de583213b32e1a0bec5f0f45
m.golovanov <m.golovanov@uk-swift.ru>
2025-07-29 09:07:05 +0300

    Добавил логгирование в файл

--- swift_django/swift_django/settings.py
+++ swift_django/swift_django/settings.py
@@ -146,3 +146,22 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "debug.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
